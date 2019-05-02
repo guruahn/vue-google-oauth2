b6b30a2afd2c1731e46a6a6556257a63e6137ba3
Jeong woo Ahn <guruahn@gmail.com>
2019-05-02 14:05:40 +0900

    add multiple option

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
+/* eslint-disable */
 var googleAuth = (function () {
 
-  function installClient () {
+  function installClient() {
     var apiUrl = 'https://apis.google.com/js/api.js'
     return new Promise((resolve) => {
       var script = document.createElement('script')
@@ -17,137 +18,145 @@ var googleAuth = (function () {
     })
   }
 
-  function initClient (config) {
+  function initClient(config) {
     return new Promise((resolve) => {
       window.gapi.load('auth2', () => {
         window.gapi.auth2.init(config)
-        .then(() => {
-          resolve(window.gapi)
-        })
+          .then(() => {
+            resolve(window.gapi)
+          })
       })
     })
-    
+
   }
-  
-  function Auth(){
-    if(!(this instanceof Auth))
+
+  function Auth() {
+    if (!(this instanceof Auth))
       return new Auth()
     this.GoogleAuth = null /* window.gapi.auth2.getAuthInstance() */
     this.isAuthorized = false
     this.isInit = false
     this.prompt = null
-    this.isLoaded = function(){
-      /* eslint-disable */
+    this.isLoaded = function () {
       console.warn('isLoaded() will be deprecated. You can use "this.$gAuth.isInit"')
       return !!this.GoogleAuth
     };
 
     this.load = (config, prompt) => {
       installClient()
-      .then(() => {
-        return initClient(config)
-      })
-      .then((gapi) => {
-        this.GoogleAuth = gapi.auth2.getAuthInstance()
-        this.isInit = true
-        this.prompt = prompt  
-        this.isAuthorized = this.GoogleAuth.isSignedIn.get()
-      })
+        .then(() => {
+          return initClient(config)
+        })
+        .then((gapi) => {
+          this.GoogleAuth = gapi.auth2.getAuthInstance()
+          this.isInit = true
+          this.prompt = prompt
+          this.isAuthorized = this.GoogleAuth.isSignedIn.get()
+        })
     };
 
     this.signIn = (successCallback, errorCallback) => {
       return new Promise((resolve, reject) => {
-        if(!this.GoogleAuth) {
-          if(typeof errorCallback === 'function') errorCallback(false)
+        if (!this.GoogleAuth) {
+          if (typeof errorCallback === 'function') errorCallback(false)
           reject(false)
           return
         }
         this.GoogleAuth.signIn()
-        .then(googleUser => {
-          if(typeof successCallback === 'function') successCallback(googleUser)
-          this.isAuthorized = this.GoogleAuth.isSignedIn.get()
-          resolve(googleUser)
-        })
-        .catch(error => {
-          if(typeof errorCallback === 'function') errorCallback(error)
-          reject(error)
-        })
+          .then(googleUser => {
+            if (typeof successCallback === 'function') successCallback(googleUser)
+            this.isAuthorized = this.GoogleAuth.isSignedIn.get()
+            resolve(googleUser)
+          })
+          .catch(error => {
+            if (typeof errorCallback === 'function') errorCallback(error)
+            reject(error)
+          })
       })
     };
 
-    this.getAuthCode =  (successCallback, errorCallback) => {
+    this.getAuthCode = (successCallback, errorCallback) => {
       return new Promise((resolve, reject) => {
-        if(!this.GoogleAuth) {
-          if(typeof errorCallback === 'function') errorCallback(false)
+        if (!this.GoogleAuth) {
+          if (typeof errorCallback === 'function') errorCallback(false)
           reject(false)
           return
         }
-        this.GoogleAuth.grantOfflineAccess({prompt: this.prompt})
-        .then(function(resp) {
-          if(typeof successCallback === 'function') successCallback(resp.code)
-          resolve(resp.code)
-        })
-        .catch(function(error) {
-          if(typeof errorCallback === 'function') errorCallback(error)
-          reject(error)
-        })
+        this.GoogleAuth.grantOfflineAccess({ prompt: this.prompt })
+          .then(function (resp) {
+            if (typeof successCallback === 'function') successCallback(resp.code)
+            resolve(resp.code)
+          })
+          .catch(function (error) {
+            if (typeof errorCallback === 'function') errorCallback(error)
+            reject(error)
+          })
       })
     };
 
     this.signOut = (successCallback, errorCallback) => {
       return new Promise((resolve, reject) => {
-        if(!this.GoogleAuth) {
-          if(typeof errorCallback === 'function') errorCallback(false)
+        if (!this.GoogleAuth) {
+          if (typeof errorCallback === 'function') errorCallback(false)
           reject(false)
           return
         }
         this.GoogleAuth.signOut()
-        .then(() => {
-          if(typeof successCallback === 'function') successCallback()
-          this.isAuthorized = false
-          resolve(true)
-        })
-        .catch(error => {
-          if(typeof errorCallback === 'function') errorCallback(error)
-          reject(error)
-        })
+          .then(() => {
+            if (typeof successCallback === 'function') successCallback()
+            this.isAuthorized = false
+            resolve(true)
+          })
+          .catch(error => {
+            if (typeof errorCallback === 'function') errorCallback(error)
+            reject(error)
+          })
       })
     };
   }
 
-  return new Auth()
+  return Auth
 })();
-  
-  
-  
-  
-function installGoogleAuthPlugin (Vue, options) {
+
+
+
+
+function installGoogleAuthPlugin(Vue, options) {
   //set config
   let GoogleAuthConfig = null
-  let GoogleAuthDefaultConfig = { scope: 'profile email', discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']}
+  let GoogleAuthDefaultConfig = { scope: 'profile email', discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'] }
+  let namesOfPropsVue = ['gAuth']
   let prompt = 'select_account'
   if (typeof options === 'object') {
     GoogleAuthConfig = Object.assign(GoogleAuthDefaultConfig, options)
-    if(options.scope) GoogleAuthConfig.scope = options.scope
-    if(options.prompt) prompt = options.prompt
-    if(!options.clientId) {
-      /* eslint-disable */
+    if (options.scope) GoogleAuthConfig.scope = options.scope
+    if (options.prompt) prompt = options.prompt
+    if (!options.clientId) {
       console.warn('clientId is required')
     }
-  }else{
+  } else {
     console.warn('invalid option type. Object type accepted only')
   }
-  
-  //Install Vue plugin
-  Vue.gAuth = googleAuth
+  if (options.names && !Array.isArray(options.names)) {
+    console.warn('invalid option.name type. Array type accepted only')
+  }
+  if (options.names) namesOfPropsVue = options.names
+  console.log('nameOfPropsVue', namesOfPropsVue)
+  namesOfPropsVue.map(name => {
+    installVuePlugin(Vue, GoogleAuthConfig, prompt, name)
+  })
+}
+
+function installVuePlugin(Vue, GoogleAuthConfig, prompt, nameOfPropsVue) {
+  Vue[nameOfPropsVue] = new googleAuth()
   Object.defineProperties(Vue.prototype, {
-    $gAuth: {
+    ['$' + nameOfPropsVue]: {
       get: function () {
-        return Vue.gAuth
+        return Vue[nameOfPropsVue]
       }
     }
   })
-  Vue.gAuth.load(GoogleAuthConfig, prompt)
+  Vue[nameOfPropsVue].load(GoogleAuthConfig, prompt)
 }
 
 export default installGoogleAuthPlugin
