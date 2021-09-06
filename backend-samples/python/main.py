a8f7107321399f9100fae0af53205c7e91185c43
SgtChrome <42889199+SgtChrome@users.noreply.github.com>
2021-09-06 17:43:52 +0200

    Update from deprecated library oauth2client to google_auth_oauthlib

--- backend-samples/python/main.py
+++ backend-samples/python/main.py
@@ -1,21 +1,32 @@
-# python example for the exchange of auth code to refresh token
-# Reference: https://oauth2client.readthedocs.io/en/latest/source/oauth2client.client.html#
+# Python example for the exchange of auth code to refresh token
+# Reference: https://google-auth-oauthlib.readthedocs.io/en/latest/reference/google_auth_oauthlib.flow.html
+from google_auth_oauthlib.flow import Flow
+from oauthlib.oauth2.rfc6749.errors import OAuth2Error
 
-from oauth2client import client
+CLIENT_SECRETS_FILE = 'YOUR_CLIENT_SECRETS_FILE'  # get this from https://console.cloud.google.com/apis/credentials
 
-CLIENT_ID = 'YOUR_CLIENT_ID'
+SCOPES = "openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"  # example scopes
 
-CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
-
+# reference for convenience
+def credentials_to_dict(credObject):
+    return {'token': credObject.token,
+            'refresh_token': credObject.refresh_token,
+            'token_uri': credObject.token_uri,
+            'client_id': credObject.client_id,
+            'client_secret': credObject.client_secret,
+            'scopes': credObject.scopes}
 
 if __name__ == "__main__":
     auth_code = 'auth_code_from_client'
 
+    flow = Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES, redirect_uri="postmessage")
     try:
-        credentials = client.credentials_from_code(CLIENT_ID, CLIENT_SECRET, scope='', code=auth_code)
-    except client.FlowExchangeError as e:
-        print(e)
-    else:
-        print('refresh_token:', credentials.refresh_token)
-        print('access_token:', credentials.access_token)
-        print('token_expiry:', credentials.token_expiry)
+        flow.fetch_token(code=auth_code)
+    except OAuth2Error:
+        # error handling, for example InvalidGrantError for malformed auth_code
+        print('An error occured')
+
+    credentials = flow.credentials
+
+    print('access_token:', credentials.token)
+    print('refresh_token:', credentials.refresh_token)
